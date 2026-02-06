f1a49d6cdcf442f322b5c154d27cdf259f1605d5
Brady Watkinson <brady.watkinson@ziphq.com>
2026-02-06 12:33:23 -0800

    chore: bump to v0.2.0 [skip-version]

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,17 @@
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import { Command } from "commander";
 import { enableCommand } from "./commands/enable.js";
 import { listCommand } from "./commands/list.js";
 import { completionCommand } from "./commands/completion.js";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pkg = JSON.parse(readFileSync(join(__dirname, "..", "package.json"), "utf-8"));
 const program = new Command();
 program
     .name("pipeline")
     .description("CLI tool to selectively enable GitHub Actions jobs for isolated testing")
-    .version("0.1.0");
+    .version(pkg.version);
 program.addCommand(enableCommand);
 program.addCommand(listCommand);
 program.addCommand(completionCommand);
